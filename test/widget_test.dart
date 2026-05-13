d5860d5b22d0812f390f01ae5bb8fb02fe8a3fda
Nimeth Sandive <insranaweera@gmail.com>
2026-05-14 00:47:19 +0530

    changed the file name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:early_learn/main.dart';
+import 'package:early_learn/main_alphabet.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
